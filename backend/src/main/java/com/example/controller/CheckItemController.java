fc0ee6e0968afe9433a7b05d63c028fd0721b813
alex351237 <530843763@qq.com>
2021-02-20 20:52:27 +0800

    客户端接口内容

---
+++ backend/src/main/java/com/example/controller/CheckItemController.java
@@ -0,0 +1,96 @@
+package com.example.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.example.constant.MessageConstant;
+import com.example.entity.PageResult;
+import com.example.entity.QueryPageBean;
+import com.example.entity.Result;
+import com.example.pojo.CheckItem;
+import com.example.service.CheckItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 检查项管理
+ */
+
+@RestController
+@RequestMapping("/checkitem")
+public class CheckItemController {
+    @Reference//查找服务
+    private CheckItemService checkItemService;
+
+    //新增检查项
+    @RequestMapping("/add")
+    public Result add(@RequestBody CheckItem checkItem){
+        try{
+            checkItemService.add(checkItem);
+        }catch (Exception e){
+            e.printStackTrace();
+            //服务调用失败
+            return new Result(false, MessageConstant.ADD_CHECKITEM_FAIL);
+        }
+        return  new Result(true, MessageConstant.ADD_CHECKITEM_SUCCESS);
+    }
+
+    //检查项分页查询
+    @RequestMapping("/findPage")
+    public PageResult findPage(@RequestBody QueryPageBean queryPageBean){
+        PageResult pageResult = checkItemService.pageQuery(queryPageBean);
+        return pageResult;
+    }
+
+    //删除检查项
+    @RequestMapping("/delete")
+    public Result delete(Integer id){
+        try{
+            checkItemService.deleteById(id);
+        }catch (Exception e){
+            e.printStackTrace();
+            //服务调用失败
+            return new Result(false, MessageConstant.DELETE_CHECKITEM_FAIL);
+        }
+        return  new Result(true, MessageConstant.DELETE_CHECKITEM_SUCCESS);
+    }
+
+    //编辑检查项
+    @RequestMapping("/edit")
+    public Result edit(@RequestBody CheckItem checkItem){
+        try{
+            checkItemService.edit(checkItem);
+        }catch (Exception e){
+            e.printStackTrace();
+            //服务调用失败
+            return new Result(false, MessageConstant.EDIT_CHECKITEM_FAIL);
+        }
+        return  new Result(true, MessageConstant.EDIT_CHECKITEM_SUCCESS);
+    }
+
+    @RequestMapping("/findById")
+    public Result findById(Integer id){
+        try{
+            CheckItem checkItem = checkItemService.findById(id);
+            return  new Result(true, MessageConstant.QUERY_CHECKITEM_SUCCESS,checkItem);
+        }catch (Exception e){
+            e.printStackTrace();
+            //服务调用失败
+            return new Result(false, MessageConstant.QUERY_CHECKITEM_FAIL);
+        }
+    }
+
+    @RequestMapping("/findAll")
+    public Result findAll(){
+        try{
+            List<CheckItem> list = checkItemService.findAll();
+            return  new Result(true, MessageConstant.QUERY_CHECKITEM_SUCCESS,list);
+        }catch (Exception e){
+            e.printStackTrace();
+            //服务调用失败
+            return new Result(false, MessageConstant.QUERY_CHECKITEM_FAIL);
+        }
+    }
+}
