06f8b2b32f0c5d4ca2999978ead298276c40b463
alex351237 <530843763@qq.com>
2021-03-15 16:33:33 +0800

    redis相关工具类

---
+++ common/src/main/java/notcom/alex/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package notcom.alex.constant;
+
+/**
+ * @version: 
+ * @description:
+ * @author: alex
+ * @create: 2021-01-18 18:34
+ * @version:1.0
+ **/
+public class RedisConstant {
+    //套餐图片所有图片名称
+    public static final String SETMEAL_PIC_RESOURCES = "setmealPicResources";
+    //套餐图片保存在数据库中的图片名称
+    public static final String SETMEAL_PIC_DB_RESOURCES = "setmealPicDbResources";
+}
